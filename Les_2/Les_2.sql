d44c1e624614a655acd9622111c3fda82eb48f21
Andrey Koryagin <53011800+koryagin2006@users.noreply.github.com>
2020-04-12 13:24:35 +0500

    Create Les_2.sql

---
+++ Les_2/Les_2.sql
@@ -0,0 +1,22 @@
+﻿SELECT * 
+FROM orders_20190822
+ORDER BY price ASC;
+
+
+DELETE LOW_PRIORITY QUICK
+  FROM orders_filter
+  WHERE price <= 0;
+
+SELECT * FROM orders_filter of WHERE price <= 0;
+
+
+SELECT COUNT(o_date) FROM
+  (
+SELECT o_date, SUM(price) s, COUNT(id_o) n
+  FROM orders_filter
+  GROUP BY o_date) AS filt;
+
+SELECT YEAR(o_date),MONTH(o_date), CEIL(SUM(price))
+  FROM orders_filter 
+  GROUP BY YEAR(o_date), MONTH(o_date)
+  ORDER BY YEAR(o_date);

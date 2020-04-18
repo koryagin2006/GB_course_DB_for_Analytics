a21aee78bf0216594d0896f858dfe4e5ef318011
Andrey Koryagin <53011800+koryagin2006@users.noreply.github.com>
2020-04-19 02:29:40 +0500

    делаю

--- Les_3/Les_3_DZ.sql
+++ Les_3/Les_3_DZ.sql
@@ -13,9 +13,10 @@ SELECT * FROM orders_20190822 o;
 
 SELECT *
   FROM (
-SELECT user_id,                  
-      TIMESTAMPDIFF(DAY,MAX(o_date),date('2017-12-31')) AS perid_from_last_ord,
-       COUNT(o.id_o) as count_orders,
-       AVG(o.price) as avg_price
+SELECT 
+  user_id,
+  TIMESTAMPDIFF(DAY,MAX(o_date),date('2017-12-31')) AS period_from_last_ord,
+  CEILING(COUNT(o.id_o)/TIMESTAMPDIFF(DAY,MIN(o.o_date),MAX(o.o_date))) AS freq,
+  ROUND(AVG(o.price),2) as avg_price
 FROM orders_20190822 o
 GROUP BY o.user_id) t;

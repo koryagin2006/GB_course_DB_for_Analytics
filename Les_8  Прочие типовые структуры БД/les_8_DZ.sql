f0758309245b7e73f40e86d119539f154c528872
Andrey Koryagin <53011800+koryagin2006@users.noreply.github.com>
2020-05-08 01:00:54 +0500

    Update les_8_DZ.sql

--- Les_8  Прочие типовые структуры БД/les_8_DZ.sql
+++ Les_8  Прочие типовые структуры БД/les_8_DZ.sql
@@ -21,45 +21,62 @@
 USE db_for_analyt;
 
 -- период базы
-SELECT MIN(o.o_date ), MAX(o.o_date ) FROM orders_20190822 o;
+SELECT DATE(MIN(o.o_date )), DATE(MAX(o.o_date )) FROM orders_20190822 o;
 
--- New, их число
+-- New, их число на '2017-01-01'
 SELECT 
-	user_id,
-	o_date
-FROM orders_20190822
-GROUP BY
-user_id
-HAVING
-COUNT(id_o) = 1;
+  user_id, 
+  'New' AS group_,
+  DATE('2017-01-01') AS date
+FROM 
+(SELECT * FROM orders_20190822 WHERE o_date < '2017-01-01') AS o2
+GROUP BY user_id
+HAVING COUNT(id_o) = 1;
 
 
--- Lost, их число
-SELECT COUNT(t.user_id)
+-- Lost, их число на '2017-01-01'
+SELECT
+  user_id, 
+  'Lost' AS group_,
+  DATE('2017-01-01') AS date
 FROM (
 SELECT 
-	o.user_id,
-	TIMESTAMPDIFF(MONTH,MAX(o_date),date('2017-12-31')) AS period_from_last_ord
-FROM orders_20190822 AS o
+	user_id,
+	TIMESTAMPDIFF(MONTH,MAX(o_date),DATE('2017-12-31')) AS period_from_last_ord
+FROM (SELECT * FROM orders_20190822 WHERE o_date < '2017-01-01') AS o2
 GROUP BY user_id
-HAVING COUNT(o.id_o >= 1)) AS t
+HAVING COUNT(id_o >= 1)) AS t
 WHERE period_from_last_ord >= 3;
 
 
-
-
-SELECT user_id, price,
-  CASE WHEN price < 1000 THEN "1"
-       WHEN price >= 1000 AND price < 5000 THEN "2"
-  ELSE "3" end  AS m
-  FROM orders_20190822;
-
-
-
+-- Vip на '2017-01-01' (avg_price>1000 & freq>2)
+SELECT
+  t2.user_id, 
+  'Vip' AS group_,
+  DATE('2017-01-01') AS date
+FROM (
 SELECT 
   user_id,
-  TIMESTAMPDIFF(DAY,MAX(o_date),date('2017-12-31')) AS period_from_last_ord,
-  CEILING(COUNT(o.id_o)/TIMESTAMPDIFF(DAY,MIN(o.o_date),MAX(o.o_date))) AS freq,
-  ROUND(AVG(o.price),2) as avg_price
+  CEILING(COUNT(o.id_o)/TIMESTAMPDIFF(MONTH,MIN(o.o_date),MAX(o.o_date))) AS freq,
+  ROUND(AVG(o.price),2) as avg_price,
+  COUNT(o.id_o) AS orders
 FROM orders_20190822 o
-GROUP BY o.user_id;
+GROUP BY o.user_id) AS t2
+WHERE t2.freq >2 AND t2.avg_price > 1000 AND t2.orders > 1;
+
+
+-- Regular на '2017-01-01' (avg_price<=1000 & freq>2)
+SELECT 
+  *
+  /*t2.user_id, 
+  'Regular' AS group_,
+  DATE('2017-01-01') AS date */
+FROM (
+SELECT 
+  user_id,
+  CEILING(COUNT(o.id_o)/TIMESTAMPDIFF(MONTH,MIN(o.o_date),MAX(o.o_date))) AS freq,
+  ROUND(AVG(o.price),2) as avg_price,
+  COUNT(o.id_o) AS orders
+FROM orders_20190822 o
+GROUP BY o.user_id) AS t2
+WHERE t2.freq >=0 AND t2.avg_price <= 1000 AND t2.orders > 1;
